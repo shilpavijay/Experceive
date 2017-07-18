0ab594f52f7d36a98f7b98104582325e9fc9d283
shilpavijay <shilpa.amberker@gmail.com>
2017-07-18 23:19:35 +0530

    pep8fixes

--- analyzer.py
+++ analyzer.py
@@ -2,13 +2,15 @@ import pandas as pd
 import logging
 import matplotlib.pyplot as plt
 
+
 def csvAnalze(file):
 	dfinit = pd.read_csv(file)
 	dfcat = dfinit.groupby('Category').sum()
-	dfcat.plot(kind = 'pie',subplots = 'True')
-	labels = ['Grocery','Food','Others','Phone Exp','Travel Exp']
+	dfcat.plot(kind='pie', subplots='True')
+	labels = ['Grocery', 'Food', 'Others', 'Phone Exp', 'Travel Exp']
 	plt.legend(labels)
 	return dfcat
+
 
 if __name__ == "__main__":
 	logging.basicConfig(filename='analyzerlog.log', filemode='w', level=logging.DEBUG)
