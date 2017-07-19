671cf49bae93246d48514fd37e97d9c6b694d2b7
shilpavijay <shilpa.amberker@gmail.com>
2017-07-19 20:14:23 +0530

    'firstvizrec'

--- analyzer.py
+++ analyzer.py
@@ -7,14 +7,23 @@ def csvAnalze(file):
 	dfinit = pd.read_csv(file)
 	dfcat = dfinit.groupby('Category').sum()
 	labels = ['Grocery', 'Food', 'Others', 'Phone Exp', 'Travel Exp']
-	dfcat.plot(kind='pie', subplots='True', autopct='%1.1f%%', 
-		labels=labels, shadow=True, startangle=90)
+	explode = (0, 0.05, 0, 0, 0)
+	colors = ['#b64b50', '#f46e6e', '#f69a9a', '#4dc9ba', '#afd2e7']
+	
+	dfcat.plot(kind='pie', subplots='True', autopct='%1.1f%%', labels=labels, 
+				shadow=True, startangle=90, explode=explode, colors=colors)
+
+	plt.subplots_adjust(left=0.13, bottom=0.09, right=0.78, top=0.9, 
+						wspace=0.2, hspace=0.2)
+
+	plt.title('Experceive', bbox={'facecolor' : '0.9', 'pad' : 3})
+	plt.show()
 	return dfcat
 
 
 if __name__ == "__main__":
-	logging.basicConfig(filename='analyzerlog.log', filemode='w', level=logging.DEBUG)
+	logging.basicConfig(filename='analyzerlog.log', filemode='w', 
+						level=logging.DEBUG)
 	logging.info('Started')
 	ret = csvAnalze('JulyExp.csv')
 	logging.debug(ret)
-	plt.show()
